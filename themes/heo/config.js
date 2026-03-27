5f40cd5b7ace6fa77dc85cca634c20d11b61bc1a
zhujinkun20110402 <139196162+zhujinkun20110402@users.noreply.github.com>
2026-03-28 01:34:22 +0800

    Update site creation date and hero section content
    Updated site creation date and modified hero section titles and notice bar content. Changed social card visibility to false.

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,13 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2026-03-28', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到小浣熊的博客', url: 'https://l-rac.top' },
+    { title: '看看关于页？', url: 'https://l-rac.top/about' },
+    { title: '艺术源于生活，高于生活', url: 'https://l-rac.top'}
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,11 +19,11 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_1: '记录生活',
+  HEO_HERO_TITLE_2: '与思考',
+  HEO_HERO_TITLE_3: 'L-RAC.TOP',
+  HEO_HERO_TITLE_4: '看看这个',
+  HEO_HERO_TITLE_5: '在Bilibili上关注我',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
@@ -116,7 +117,7 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
