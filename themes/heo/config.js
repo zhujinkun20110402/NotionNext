6f09d9162166062cb9f579e53c4890e7d477ef43
zhujinkun20110402 <139196162+zhujinkun20110402@users.noreply.github.com>
2026-03-28 01:39:34 +0800

    Update hero section links and cover image

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'L-RAC.TOP',
   HEO_HERO_TITLE_4: '看看这个',
   HEO_HERO_TITLE_5: '在Bilibili上关注我',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_LINK: 'https://space.bilibili.com/3493276246084510?spm_id_from=333.40164.0.0',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -36,7 +36,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'https://www.logo9.net/userfiles/images/9bilibili1.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
